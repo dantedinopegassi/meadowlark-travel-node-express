17c8ad231a37453a66746164eb7fffc5da1daf62
dinopegassi <nmbost@gmail.com>
2024-06-03 07:30:29 +0930

    ch3 pg28

--- src/meadowlarkENTRYPOINT.js
+++ src/meadowlarkENTRYPOINT.js
@@ -1,39 +1,45 @@
-const handleBars = require("express-handlebars")
+const x = require("express");
+const hb = require("express-handlebars");
 
-const app = require('express')();
-app.engine('handlebars', handleBars({
-    defaultLayout: "main",
-}))
+const app = x();
 
-app.set('view engine', "handlebars");
+app.set("views", __dirname + "/views");
+app.engine("handlebars", hb.engine({
+    defaultLayout: "template",
+  })
+);
+app.set("view engine", "handlebars");
 
 const port = process.env.PORT || 24900; // el puerto va a ser env.PORT o el 24900
 
-app.get('/' ,(req, res) => {
-    res.type('text/plain');
-    res.send("el home de meadowlark");
-})
+// manera de rutear sin handlebars
+app.get("/a", (req, res) => {
+  res.type("text/plain");
+  res.send("el 'a' de meadowlark");
+});
 
-app.get("/about", (req, res ) => {
-    res.type('text/plain');
-    res.send("el ACERCA DE de meadowlark");
-})
+// manera de rutear con handlebars
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 app.use((req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 Not Found');
-})
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 Not Found");
+});
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 Internal Server Error');
-})
+  console.error(err);
+  res.type("text/plain");
+  res.status(500);
+  res.send("500 Internal Server Error");
+});
 
 app.listen(port, () => {
-    console.log(
-        `arranco el sv en el puerto ${port}, ctrl-c para liquidarlo`
-    );
-})
+  console.log(`arranco el sv en el puerto ${port}, ctrl-c para liquidarlo`);
+});
